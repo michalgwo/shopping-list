60ebeb0b2e1a2a77d1599fe3544468c2e351428e
michalgwo <114614618+michalgwo@users.noreply.github.com>
2022-11-18 18:44:25 +0100

    Initiate recycler view

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.shoppinglist.adapters.ShoppingListAdapter;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private Button bAdd;
     private EditText etNewItem;
     private RecyclerView rvItemList;
+    private ShoppingListAdapter adapter;
 
     private void init() {
         bAdd = findViewById(R.id.bAdd);
@@ -24,5 +30,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        initRecyclerView();
+    }
+
+
+    private void initRecyclerView() {
+        adapter = new ShoppingListAdapter(new ArrayList<>());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvItemList.setLayoutManager(layoutManager);
+        rvItemList.setAdapter(adapter);
     }
 }
