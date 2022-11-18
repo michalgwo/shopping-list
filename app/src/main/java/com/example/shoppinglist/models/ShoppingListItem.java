4b0c030562188db55ae0d36e094d1653ffb22cbf
michalgwo <114614618+michalgwo@users.noreply.github.com>
2022-11-18 18:48:36 +0100

    Created shopping list item

---
+++ app/src/main/java/com/example/shoppinglist/models/ShoppingListItem.java
@@ -0,0 +1,27 @@
+package com.example.shoppinglist.models;
+
+public class ShoppingListItem {
+    private String name;
+    private long time;
+
+    public ShoppingListItem(String name, long time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
