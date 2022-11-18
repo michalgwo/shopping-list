cfcc5ff85e28de8e0644e79b0c7da635fe64f378
michalgwo <114614618+michalgwo@users.noreply.github.com>
2022-11-18 18:26:30 +0100

    Get components in MainActivity.java

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bAdd;
+    private EditText etNewItem;
+    private RecyclerView rvItemList;
+
+    private void init() {
+        bAdd = findViewById(R.id.bAdd);
+        etNewItem = findViewById(R.id.etNewItem);
+        rvItemList = findViewById(R.id.rvItemList);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
 }
